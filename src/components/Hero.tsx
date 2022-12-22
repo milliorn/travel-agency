316ebd4057db053987dc850b39bdb3d6071bfe7e
milliorn <scottmilliorn@gmail.com>
2022-12-21 21:06:03 -0800

    div, p

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,10 +11,12 @@ const Hero = () => {
       {/* overlay */}
       <div className="bg-black/30 absolute top-0 left-0 w-full h-screen" />
       <div className="absolute top-0 w-full h-full flex flex-col justify-center text-white">
-        <div>
+        <div className="md:left[10%] max-w-[1100px] m-auto absolute p-4">
           <p>Exclusive</p>
-          <h1>Private &amp; Getaways</h1>
-          <p>
+          <h1 className="font-bold text-5xl md:text-7xl drop-shadow-2xl">
+            Private &amp; Getaways
+          </h1>
+          <p className="max-w-[600px] drop-shadow-2xl py-2 text-xl">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit.
             Perspiciatis odit nulla itaque tempore optio incidunt nesciunt amet
             delectus ducimus dolore explicabo, odio quia perferendis doloribus
