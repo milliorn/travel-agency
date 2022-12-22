0c111492d75effe9a8516226881515d1229fe9a6
milliorn <scottmilliorn@gmail.com>
2022-12-21 20:52:31 -0800

    overlay

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
-    <div>
-      <img src="https://source.unsplash.com/random/2560x1440/?beach" alt="beach" />
+    <div className="w-full h-screen">
+      <img
+        className="top-0 left-0 w-full h-screen object-cover"
+        src="https://source.unsplash.com/random/2560x1440/?beach"
+        alt="beach"
+      />
+      {/* overlay */}
+      <div className="bg-black/30 absolute top-0 left-0 w-full h-screen" />
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
