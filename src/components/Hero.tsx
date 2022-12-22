16901f947d1135ff132c89c8829194fcd239a04a
milliorn <scottmilliorn@gmail.com>
2022-12-21 21:02:19 -0800

    text

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,6 +10,19 @@ const Hero = () => {
       />
       {/* overlay */}
       <div className="bg-black/30 absolute top-0 left-0 w-full h-screen" />
+      <div className="absolute top-0 w-full h-full flex flex-col justify-center text-white">
+        <div>
+          <p>Exclusive</p>
+          <h1>Private &amp; Getaways</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Perspiciatis odit nulla itaque tempore optio incidunt nesciunt amet
+            delectus ducimus dolore explicabo, odio quia perferendis doloribus
+            facere, tempora, ab quod maiores!
+          </p>
+          <button>RSVP Now</button>
+        </div>
+      </div>
     </div>
   );
 };
