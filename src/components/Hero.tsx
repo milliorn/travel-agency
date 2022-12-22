a80994a6370c7bfa7bbad6af0d27b0155b494ac6
milliorn <scottmilliorn@gmail.com>
2022-12-21 20:41:43 -0800

    random beach image

---
+++ src/components/Hero.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+      <img src="https://source.unsplash.com/random/2560x1440/?beach" alt="beach" />
+    </div>
+  )
+}
+
+export default Hero
