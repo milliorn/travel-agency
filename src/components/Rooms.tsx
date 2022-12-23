18b8b948210cd05f29a61966d9dd31aa664a8a5f
Scott Milliorn <scottmilliorn@gmail.com>
2022-12-23 00:08:32 -0800

    Rooms (#13)
    * h3
    
    * img src
    
    * rooms

---
+++ src/components/Rooms.tsx
@@ -0,0 +1,37 @@
+function Rooms(): JSX.Element {
+  return (
+    <div className="max-w-[1400px] h-[500px] bg-blue-100 mx-auto my-20 pt-16 lg:mb-[20%] md:mb-[35%] px-4 grid lg:grid-cols-3 gap-4">
+      <div className="lg:top-20 relative lg:col-span-1 col-span-2">
+        <h3 className="text-2xl font-bold">Fine Interior Rooms</h3>
+        <p className="pt-4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque
+          possimus temporibus maiores assumenda dolor reiciendis fugit! Iusto
+          doloribus fuga commodi ex cum atque voluptatem? Dolorem accusamus quos
+          possimus fugiat asperiores?
+        </p>
+      </div>
+      <div className="grid grid-cols-2 col-span-2 gap-2">
+        <img
+          className="object-cover w-full h-full"
+          loading="lazy"
+          src="https://source.unsplash.com/random/2560x1440/?luxury+bathroom"
+          alt="/"
+        />
+        <img
+          className="object-cover w-full h-full row-span2"
+          loading="lazy"
+          src="https://source.unsplash.com/random/2560x1440/?luxury+bedroom"
+          alt="/"
+        />
+        <img
+          className="object-cover w-full h-full"
+          loading="lazy"
+          src="https://source.unsplash.com/random/2560x1440/?luxury+hotel"
+          alt="/"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Rooms;
