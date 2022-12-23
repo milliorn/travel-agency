4764f76ca64bbe58580161dfc49b6094136d9fc3
milliorn <scottmilliorn@gmail.com>
2022-12-22 21:07:39 -0800

    styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,11 +2,17 @@ import { HiMenuAlt3 } from "react-icons/hi";
 
 function Navbar(): JSX.Element {
   return (
-    <div>
-      <h1>Experiences</h1>
-      <HiMenuAlt3 size={25} />
-      <div>
-        
+    <div className="absolute w-full flex justify-between p-4 items-center">
+      <h1 className="text-white font-bold text-2xl z-20">Adventures</h1>
+      <HiMenuAlt3 size={25} className="z-20 text-white cursor-pointer" />
+      <div className="fixed text-gray-300 left-0 top-0 w-full h-screen bg-black/90 px-4 py-7 flex-col z-10">
+        <ul className="flex flex-col fixed w-full h-full items-center justify-center">
+          <li className="font-bold text-3xl p-8">Home</li>
+          <li className="font-bold text-3xl p-8">Destinations</li>
+          <li className="font-bold text-3xl p-8">Reservations</li>
+          <li className="font-bold text-3xl p-8">Amenities</li>
+          <li className="font-bold text-3xl p-8">Rooms</li>
+        </ul>
       </div>
     </div>
   );
