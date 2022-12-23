f0cb8307bbb1d3cbc98d2485eeb49fe57642a860
milliorn <scottmilliorn@gmail.com>
2022-12-22 22:01:53 -0800

    offers

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Offers from "./components/Offers";
 
 export default function App(): JSX.Element {
   return (
     <>
       <Navbar />
       <Hero />
+      <Offers />
     </>
   );
 }
