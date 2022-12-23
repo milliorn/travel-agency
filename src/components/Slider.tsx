3a94e77d8bb4a57137eb92731603f250494372f5
milliorn <scottmilliorn@gmail.com>
2022-12-23 15:56:51 -0800

    slides

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,7 +1,24 @@
+const slides = [
+  {
+    url: "https://source.unsplash.com/random/2560x1440/?lobster",
+    title: "Lobster",
+  },
+  {
+    url: "https://source.unsplash.com/random/2560x1440/?sushi",
+    title: "Sushi",
+  },
+  {
+    url: "https://source.unsplash.com/random/2560x1440/?pasta",
+    title: "Pasta",
+  },
+  {
+    url: "https://source.unsplash.com/random/2560x1440/?salmon",
+    title: "Salmon",
+  },
+];
+
 function Slider(): JSX.Element {
-  return (
-    <div>Slider</div>
-  )
+  return <div></div>;
 }
 
-export default Slider
+export default Slider;
