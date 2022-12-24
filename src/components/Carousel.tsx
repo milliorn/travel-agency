0a5f89dcb77b5cb1085a9608c812729c2a3d9547
milliorn <scottmilliorn@gmail.com>
2022-12-23 23:17:34 -0800

    Carousel width, height

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -4,7 +4,7 @@ import { RxDotFilled } from "react-icons/rx";
 
 const slides = [
   {
-    url: "https://source.unsplash.com/random/2560x1440/?dinner+lobster",
+    url: "https://source.unsplash.com/random/2560x1440/?lobster",
   },
   {
     url: "https://source.unsplash.com/random/2560x1440/?sushi",
@@ -13,7 +13,7 @@ const slides = [
     url: "https://source.unsplash.com/random/2560x1440/?pasta",
   },
   {
-    url: "https://source.unsplash.com/random/2560x1440/?salmon+dinner",
+    url: "https://source.unsplash.com/random/2560x1440/?halibut+dinner",
   },
 ] as const;
 
@@ -37,7 +37,7 @@ export default function Carousel(): JSX.Element {
   }
 
   return (
-    <div className="max-w-[1400px] h-[580px] w-11/12	m-auto py-16 px-4 relative group">
+    <div className="max-w-7xl h-[720px] w-11/12	m-auto py-16 px-4 relative group">
       <div
         className="w-full h-full rounded-2xl bg-center bg-cover duration-500"
         style={{ backgroundImage: `url(${slides[index].url})` }}
