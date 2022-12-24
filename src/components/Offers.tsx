41d6691e8432175b4bfdaa0a8e348e14fae0c88c
milliorn <scottmilliorn@gmail.com>
2022-12-23 22:13:42 -0800

    media queries, fixes

--- src/components/Offers.tsx
+++ src/components/Offers.tsx
@@ -1,10 +1,10 @@
 function Offers(): JSX.Element {
   return (
     <div className="max-w-[900px] m-auto px-4 py-12 flex flex-wrap justify-between">
-      <p className="text-lg font-bold text-gray-700">Competitive Rates</p>
-      <p className="text-lg font-bold text-gray-700">Flexibility</p>
-      <p className="text-lg font-bold text-gray-700">Incentives</p>
-      <p className="text-lg font-bold text-gray-700">Trip Insurance</p>
+      <p className="sm:text-lg font-bold text-gray-700">Competitive Rates</p>
+      <p className="sm:text-lg font-bold text-gray-700">Flexibility</p>
+      <p className="sm:text-lg font-bold text-gray-700">Incentives</p>
+      <p className="sm:text-lg font-bold text-gray-700">Trip Insurance</p>
     </div>
   );
 }
