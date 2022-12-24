e6f7137cf57e0bc2bdafa0702a18f26d7b01837f
milliorn <scottmilliorn@gmail.com>
2022-12-23 21:35:12 -0800

    Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,25 +1,25 @@
+import React from "react";
+
 function Hero(): JSX.Element {
   return (
     <div className="w-full h-screen">
       <img
-        alt="beach"
-        className="top-0 left-0 w-full h-screen object-cover bg-black"
+        className="top-0 left-0 w-full h-screen object-cover"
         src="https://source.unsplash.com/random/2560x1440/?beach"
         loading="lazy"
+        alt="/"
       />
-      {/* overlay */}
       <div className="bg-black/30 absolute top-0 left-0 w-full h-screen" />
       <div className="absolute top-0 w-full h-full flex flex-col justify-center text-white">
-        <div className="md:left[10%] max-w-[1100px] m-auto absolute p-4">
-          <p>Exclusive</p>
+        <div className="md:left-[10%] max-w-[1100px] m-auto absolute p-4">
+          <p>Panoptic</p>
           <h1 className="font-bold text-5xl md:text-7xl drop-shadow-2xl">
             Private Resorts &amp; Paradises
           </h1>
           <p className="max-w-[600px] drop-shadow-2xl py-2 text-xl">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Perspiciatis odit nulla itaque tempore optio incidunt nesciunt amet
-            delectus ducimus dolore explicabo, odio quia perferendis doloribus
-            facere, tempora, ab quod maiores!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
+            facere incidunt inventore quas sint dolore hic laudantium quis
+            maxime quam.
           </p>
           <button className="bg-white text-black">RSVP Now</button>
         </div>
