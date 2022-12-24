df28c807385f5327b9a11f029430e59e0175c558
milliorn <scottmilliorn@gmail.com>
2022-12-23 16:57:46 -0800

    Carousel

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 import Offers from "./components/Offers";
 import Plan from "./components/Plan";
 import Rooms from "./components/Rooms";
-import Slider from "./components/Slider";
+import Carousel from "./components/Carousel";
 
 export default function App(): JSX.Element {
   return (
@@ -13,7 +13,7 @@ export default function App(): JSX.Element {
       <Offers />
       <Plan />
       <Rooms />
-      <Slider />
+      <Carousel />
     </>
   );
 }
---
+++ src/components/Carousel.tsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
+import { RxDotFilled } from "react-icons/rx";
+
+
+export default function Carousel(): JSX.Element {
+  const slides = [
+    {
+      url: "https://source.unsplash.com/random/2560x1440/?dinner+lobster",
+      title: "Lobster",
+    },
+    {
+      url: "https://source.unsplash.com/random/2560x1440/?sushi",
+      title: "Sushi",
+    },
+    {
+      url: "https://source.unsplash.com/random/2560x1440/?pasta",
+      title: "Pasta",
+    },
+    {
+      url: "https://source.unsplash.com/random/2560x1440/?salmon+dinner",
+      title: "Salmon",
+    },
+  ] as const;
+
+  const [index, setindex] = useState(0);
+
+  function prevSlide(): void {
+    const isFirstSlide = index === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : index - 1;
+    setindex(newIndex);
+  }
+
+  function nextSlide(): void {
+    const isLastSlide = index === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : index + 1;
+    setindex(newIndex);
+  }
+
+  function goToSlide(slideIndex: number): void {
+    setindex(slideIndex);
+  }
+
+  return (
+    <div className="max-w-[1400px] h-[580px] w-full m-auto py-16 px-4 relative group">
+      <div
+        className="w-full h-full rounded-2xl bg-center bg-cover duration-500"
+        style={{ backgroundImage: `url(${slides[index].url})` }}
+      ></div>
+      {/* Left Arrow */}
+      <div className="absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 group-hover:bg-black/20 text-white cursor-pointer">
+        <BsChevronCompactLeft onClick={prevSlide} size={30} />
+      </div>
+      {/* Right Arrow */}
+      <div className="absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 group-hover:bg-black/20 text-white cursor-pointer">
+        <BsChevronCompactRight onClick={nextSlide} size={30} />
+      </div>
+
+      <div className="flex top-4 justify-center py-2">
+        {slides.map((slide, slideIndex) => (
+          <div
+            className="text-2xl cursor-pointer"
+            key={slideIndex}
+            onClick={() => goToSlide(slideIndex)}
+          >
+            <RxDotFilled />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
